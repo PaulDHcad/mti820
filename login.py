b0e5725f0731839adc9a2ee07dd1c30031f696e1
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-23 17:08:58 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,59 +1,81 @@
 import streamlit as st
+import csv
 import hashlib
 import os
+import random
 
 # Define the path to the user storage file
-USER_STORAGE_FILE = 'users.txt'
-
+USER_STORAGE_FILE = 'users.csv'
 # If the user storage file doesn't exist, create an empty file
 if not os.path.isfile(USER_STORAGE_FILE):
-    with open(USER_STORAGE_FILE, 'w') as f:
-        pass
+    with open(USER_STORAGE_FILE, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "name", "surname", "username", "location", "password", "email", "idfavoritegenre", "birthyear])
 
 # Define a function to hash the password
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
+# Define a function to check if an ID number already exists in the CSV file
+def id_number_exists(id_number):
+    with open(USER_STORAGE_FILE, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[0] == str(id_number):
+                return True
+    return False
+                         
+# Generate a random ID number that is not already in the CSV file
+def new_id() :
+    while True:
+        id_number = random.randint(100000, 999999)
+        if not id_number_exists(id_number):
+    return id_number
+                         
 # Define a function to check if a username exists in the user storage file
 def username_exists(username):
-    with open(USER_STORAGE_FILE, 'r') as f:
-        for line in f:
-            stored_username, _, _, _ = line.strip().split(',')
-            if username == stored_username:
+    with open(USER_STORAGE_FILE, 'r', newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            if username == row[3]:
                 return True
     return False
 
 # Define a function to check if a email is already used in the user storage file
 def email_exists(email):
-    with open(USER_STORAGE_FILE, 'r') as f:
-        for line in f:
-            _, _, stored_email, _ = line.strip().split(',')
-            if email == stored_email:
+    with open(USER_STORAGE_FILE, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if row[6] == str(email):
                 return True
     return False
 
 # Define a function to add a new user to the user storage file
-def add_user(username, password, email, location):
-    with open(USER_STORAGE_FILE, 'a') as f:
-        f.write('{},{},{},{}\n'.format(username, hash_password(password), email, location))
+def add_user(name, surname, username, password, email, location, birthyear,):
+    with open(USER_STORAGE_FILE, 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow([new_id(), name, surname, username, location, hash_password(password), email, "", birthyear])
 
 # Define a function to check if the login credentials are correct
 def check_credentials(username, password):
-    with open(USER_STORAGE_FILE, 'r') as f:
-        for line in f:
-            stored_username, stored_password, _, _ = line.strip().split(',')
-            if username == stored_username and hash_password(password) == stored_password:
+    with open(USER_STORAGE_FILE, 'r', newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            if username == row[3] and hash_password(password) == row[5]:
                 return True
     return False
 
-# Define a function to retreive user data
-def retreive_userdata(username) :
-    with open(USER_STORAGE_FILE, 'r') as f:
-        for line in f:
-            stored_username, _, stored_email, stored_location = line.strip().split(',')
-            if username == stored_username:
-                return (stored_username, stored_email, stored_location)
-    return False
+# Define a function to get the user's details
+def get_user_details(username):
+    with open(USER_STORAGE_FILE, 'r', newline='') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            if username == row[3]:
+                return row
+    return None
 
 # Add a title
 st.title("Se connecter ou s'inscrire")
