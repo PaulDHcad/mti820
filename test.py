fcbfa3e6f67fe0b4f1c62d105953a00e8bae128c
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-25 16:12:53 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import urllib.request
+import csv
+
+# Set the URL of the raw CSV file in your GitHub repository
+csv_url = "https://raw.githubusercontent.com/username/repository-name/main/filename.csv"
+
+# Use urllib to read the contents of the CSV file from the URL
+response = urllib.request.urlopen(csv_url)
+
+# Use csv.reader to parse the contents of the CSV file
+reader = csv.reader(response.read().decode('utf-8').splitlines())
+
+# Iterate through the rows of the CSV file and display them in a Streamlit table
+for row in reader:
+    st.write(row)
