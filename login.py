2c5ea4cc13dbd02a9bc39c581e3ac24fbdb7e3d5
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-27 16:42:19 -0400

    Update login.py

--- login.py
+++ login.py
@@ -184,73 +184,67 @@ def main():
                 st.write("Vos genre favoris:", userdata[7])
                 col1, col2 = st.columns(2)
                 with col1:
-                    selected = st.checkbox(Genres[0])
-                    if selected:
-                        selected_genre.append(Genres[0])
-
-                    selected = st.checkbox(Genres[1])
-                    if selected:
-                        selected_genre.append(Genres[1])
-
-                    selected = st.checkbox(Genres[2])
-                    if selected:
-                        selected_genre.append(Genres[2])
-
-                    selected = st.checkbox(Genres[3])
-                    if selected:
-                        selected_genre.append(Genres[3])                  
-
-                    selected = st.checkbox(Genres[4])
-                    if selected:
-                        selected_genre.append(Genres[4])                   
-
-                    selected = st.checkbox(Genres[5])
-                    if selected:
-                        selected_genre.append(Genres[5])     
-
-                    selected = st.checkbox(Genres[6])
-                    if selected:
-                        selected_genre.append(Genres[6])                    
-
-                    selected = st.checkbox(Genres[7])
-                    if selected:
-                        selected_genre.append(Genres[7])
-
+                    option1 = st.checkbox("Animation/Animé")
+                    option2 = st.checkbox("Aventure")
+                    option3 = st.checkbox("Romantique")
+                    option4 = st.checkbox("Comédie")
+                    option5 = st.checkbox("Action")
+                    option6 = st.checkbox("Familial")
+                    option7 = st.checkbox("Dramatique")
+                    option8 = st.checkbox("Crimes")       
+                    option9 = st.checkbox("Fantaisie")        
                 with col2:
-                    selected = st.checkbox(Genres[8])
-                    if selected:
-                        selected_genre.append(Genres[8])
+                    option10 = st.checkbox("Science fiction")
+                    option11 = st.checkbox("Thriller")
+                    option12 = st.checkbox("Musical")
+                    option13 = st.checkbox("Horreur")
+                    option14 = st.checkbox("Documentaire")
+                    option15 = st.checkbox("Mystère")
+                    option16 = st.checkbox("Western") 
+                    option17 = st.checkbox("Guerre")        
+                    option18 = st.checkbox("Film de télévision")   
 
-                    selected = st.checkbox(Genres[9])
-                    if selected:
-                        selected_genre.append(Genres[9])
-
-                    selected = st.checkbox(Genres[10])
-                    if selected:
-                        selected_genre.append(Genres[10])
-
-                    selected = st.checkbox(Genres[11])
-                    if selected:
-                        selected_genre.append(Genres[11])                  
-
-                    selected = st.checkbox(Genres[12])
-                    if selected:
-                        selected_genre.append(Genres[12])                   
-
-                    selected = st.checkbox(Genres[13])
-                    if selected:
-                        selected_genre.append(Genres[13])     
-
-                    selected = st.checkbox(Genres[14])
-                    if selected:
-                        selected_genre.append(Genres[14])                    
-
-                    selected = st.checkbox(Genres[15])
-                    if selected:
-                        selected_genre.append(Genres[15])                   
-
-                if st.button("Valider la saisie"):
-                    writer.writerow([_, _, _, _, _, _, _, selected_genre, _])
+                # Define a validation button to write the selected options into a variable
+                if st.button('Mettre à jour les préférences'):
+                    selected_options = []
+                    if option1:
+                        selected_options.append("Animation/Animé")
+                    if option2:
+                        selected_options.append("Aventure")
+                    if option3:
+                        selected_options.append("Romantique")
+                    if option4:
+                        selected_options.append("Comédie")
+                    if option5:
+                        selected_options.append("Action")
+                    if option6:
+                        selected_options.append("Familial")
+                    if option7:
+                        selected_options.append("Dramatique")
+                    if option8:
+                        selected_options.append("Crimes")
+                    if option9:
+                        selected_options.append("Fantaisie")
+                    if option10:
+                        selected_options.append("Science fiction")
+                    if option11:
+                        selected_options.append("Thriller")
+                    if option12:
+                        selected_options.append("Musical")
+                    if option13:
+                        selected_options.append("Horreur")
+                    if option14:
+                        selected_options.append("Documentaire")   
+                    if option15:
+                        selected_options.append("Mystère")
+                    if option16:
+                        selected_options.append("Western") 
+                    if option17:
+                        selected_options.append("Guerre")   
+                    if option18:
+                        selected_options.append("Film de télévision")              
+                    st.write(f'Selected options: {selected_options}')                   
+                    #writer.writerow([_, _, _, _, _, _, _, selected_genre, _])
 
             else:
                 st.error("Nom d'utilisateur et/ou mot de passe incorrect(s).")
