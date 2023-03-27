471ec852aee781f68ce4a6e702cb56a9fd37d0a6
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-27 15:08:10 -0400

    Update login.py

--- login.py
+++ login.py
@@ -95,160 +95,165 @@ def get_user_details(username):
                 return row
     return None
 
-# Add a title
-st.title("Se connecter ou s'inscrire")
+def main():
+    
+    # Add a title
+    st.title("Se connecter ou s'inscrire")
 
-# Add radio buttons to select login or sign up
-choice = st.radio("Sélectionner :", ("Se connecter", "S'inscrire"))
+    # Add radio buttons to select login or sign up
+    choice = st.radio("Sélectionner :", ("Se connecter", "S'inscrire"))
 
-# If the user selects sign up
-if choice == "S'inscrire":
-    # Add user input fields for username, password, and email
-    input_name = st.text_input("Prénom", value="")
-    input_surname = st.text_input("Nom", value="")
-    input_username = st.text_input("Nouveau nom d'utilisateur", value="")
-    input_password = st.text_input("Nouveau mot de passe", value="", type="password")
-    input_email = st.text_input("Adresse courriel", value="")
-    input_location = st.selectbox("Select a country", Countries)
-    input_birthyear = st.number_input("Année de naissance", value=2000, step=1)
+    # If the user selects sign up
+    if choice == "S'inscrire":
+        # Add user input fields for username, password, and email
+        input_name = st.text_input("Prénom", value="")
+        input_surname = st.text_input("Nom", value="")
+        input_username = st.text_input("Nouveau nom d'utilisateur", value="")
+        input_password = st.text_input("Nouveau mot de passe", value="", type="password")
+        input_email = st.text_input("Adresse courriel", value="")
+        input_location = st.selectbox("Select a country", Countries)
+        input_birthyear = st.number_input("Année de naissance", value=2000, step=1)
 
-    # Add a button to submit the sign up information
-    if st.button("S'inscrire"):
-        if input_name=="":
-            st.error('Le champ Prénom est vide.')
-                     
-        else:
-            if input_surname=="":
-                st.error('Le champ Nom est vide.')
-                     
+        # Add a button to submit the sign up information
+        if st.button("S'inscrire"):
+            if input_name=="":
+                st.error('Le champ Prénom est vide.')
+
             else:
-                if input_username=="":
-                    st.error("Le champ Nom d'utilisateur est vide.")
-                     
+                if input_surname=="":
+                    st.error('Le champ Nom est vide.')
+
                 else:
-                    if input_password=="":
-                        st.error('Le champ Mot de passe est vide.')
-                     
+                    if input_username=="":
+                        st.error("Le champ Nom d'utilisateur est vide.")
+
                     else:
-                        if input_email=="":
-                            st.error('Le champ Adresse courriel est vide.')
-                     
+                        if input_password=="":
+                            st.error('Le champ Mot de passe est vide.')
+
                         else:
-                            if input_location=="-- Sélectionner un pays parmi la liste --":
-                                st.error('Veuillez choisir un pays parmi la liste dans le champ Pays')
-                     
+                            if input_email=="":
+                                st.error('Le champ Adresse courriel est vide.')
+
                             else:
-                                if input_birthyear=="" or input_birthyear < 1900 or input_birthyear > 2023:
-                                    st.error('Veuillez entrer une année correcte dans le champ Année de naissance')
-                     
+                                if input_location=="-- Sélectionner un pays parmi la liste --":
+                                    st.error('Veuillez choisir un pays parmi la liste dans le champ Pays')
+
                                 else:
-                                    if username_exists(input_username):
-                                        st.error("Nom d'utilisateur déjà utilisé. Veuillez en choisir un autre.")
+                                    if input_birthyear=="" or input_birthyear < 1900 or input_birthyear > 2023:
+                                        st.error('Veuillez entrer une année correcte dans le champ Année de naissance')
 
                                     else:
-                                        if email_exists(input_email):
-                                            st.error("Cette adresse courriel est déjà associée à un compte.")
-                                        else: 
-                                            add_user(input_name, input_surname, input_username, input_password, input_email, input_location, input_birthyear)
-                                            st.success("Inscription effectuée avec succès.")
-                                            #st.experimental_rerun()
-                
-# If the user selects login
-elif choice == "Se connecter":
-    # Add user input fields for username and password
-    username = st.text_input("Nom d'utilisateur")
-    password = st.text_input("Mot de passe", type="password")
+                                        if username_exists(input_username):
+                                            st.error("Nom d'utilisateur déjà utilisé. Veuillez en choisir un autre.")
 
-    # Add a button to submit the login credentials
-    if st.button("Se connecter"):
-        if check_credentials(username, password):
-            
-            st.success("Connecté en temps que {}".format(username))
-            selected_genre = []
-            
-            # Add the rest of your application logic here
-            userdata = get_user_details(username)
-            st.title("Vos informations")
-            st.write("Votre ID :", userdata[0])
-            st.write("Votre prénom :", userdata[1])
-            st.write("Votre nom :", userdata[2])
-            st.write("Votre nom d'utilisateur :", userdata[3])
-            st.write("Votre adresse courriel :", userdata[6])
-            st.write("Votre localisation :", userdata[4])
-            st.write("Votre année de naissance :", userdata[8])
-            
-            # Option of favorites genres
-            st.title("Favourites genres :")
-            st.write("Vos genre favoris:", userdata[7])
-            col1, col2 = st.columns(2)
-            with col1:
-                selected = st.checkbox(Genres[0])
-                if selected:
-                    selected_genre.append(Genres[0])
-                    
-                selected = st.checkbox(Genres[1])
-                if selected:
-                    selected_genre.append(Genres[1])
-                    
-                selected = st.checkbox(Genres[2])
-                if selected:
-                    selected_genre.append(Genres[2])
-                    
-                selected = st.checkbox(Genres[3])
-                if selected:
-                    selected_genre.append(Genres[3])                  
-                    
-                selected = st.checkbox(Genres[4])
-                if selected:
-                    selected_genre.append(Genres[4])                   
-                    
-                selected = st.checkbox(Genres[5])
-                if selected:
-                    selected_genre.append(Genres[5])     
-                    
-                selected = st.checkbox(Genres[6])
-                if selected:
-                    selected_genre.append(Genres[6])                    
-                    
-                selected = st.checkbox(Genres[7])
-                if selected:
-                    selected_genre.append(Genres[7])
-                    
-            with col2:
-                selected = st.checkbox(Genres[8])
-                if selected:
-                    selected_genre.append(Genres[8])
-                    
-                selected = st.checkbox(Genres[9])
-                if selected:
-                    selected_genre.append(Genres[9])
-                    
-                selected = st.checkbox(Genres[10])
-                if selected:
-                    selected_genre.append(Genres[10])
-                    
-                selected = st.checkbox(Genres[11])
-                if selected:
-                    selected_genre.append(Genres[11])                  
-                    
-                selected = st.checkbox(Genres[12])
-                if selected:
-                    selected_genre.append(Genres[12])                   
-                    
-                selected = st.checkbox(Genres[13])
-                if selected:
-                    selected_genre.append(Genres[13])     
-                    
-                selected = st.checkbox(Genres[14])
-                if selected:
-                    selected_genre.append(Genres[14])                    
-                    
-                selected = st.checkbox(Genres[15])
-                if selected:
-                    selected_genre.append(Genres[15])                   
+                                        else:
+                                            if email_exists(input_email):
+                                                st.error("Cette adresse courriel est déjà associée à un compte.")
+                                            else: 
+                                                add_user(input_name, input_surname, input_username, input_password, input_email, input_location, input_birthyear)
+                                                st.success("Inscription effectuée avec succès.")
+                                                #st.experimental_rerun()
 
-            if st.button("Valider la saisie"):
-                writer.writerow([_, _, _, _, _, _, _, selected_genre, _])
+    # If the user selects login
+    elif choice == "Se connecter":
+        # Add user input fields for username and password
+        username = st.text_input("Nom d'utilisateur")
+        password = st.text_input("Mot de passe", type="password")
+
+        # Add a button to submit the login credentials
+        if st.button("Se connecter"):
+            if check_credentials(username, password):
+
+                st.success("Connecté en temps que {}".format(username))
+                selected_genre = []
+
+                # Add the rest of your application logic here
+                userdata = get_user_details(username)
+                st.title("Vos informations")
+                st.write("Votre ID :", userdata[0])
+                st.write("Votre prénom :", userdata[1])
+                st.write("Votre nom :", userdata[2])
+                st.write("Votre nom d'utilisateur :", userdata[3])
+                st.write("Votre adresse courriel :", userdata[6])
+                st.write("Votre localisation :", userdata[4])
+                st.write("Votre année de naissance :", userdata[8])
+
+                # Option of favorites genres
+                st.title("Favourites genres :")
+                st.write("Vos genre favoris:", userdata[7])
+                col1, col2 = st.columns(2)
+                with col1:
+                    selected = st.checkbox(Genres[0])
+                    if selected:
+                        selected_genre.append(Genres[0])
+
+                    selected = st.checkbox(Genres[1])
+                    if selected:
+                        selected_genre.append(Genres[1])
+
+                    selected = st.checkbox(Genres[2])
+                    if selected:
+                        selected_genre.append(Genres[2])
+
+                    selected = st.checkbox(Genres[3])
+                    if selected:
+                        selected_genre.append(Genres[3])                  
+
+                    selected = st.checkbox(Genres[4])
+                    if selected:
+                        selected_genre.append(Genres[4])                   
+
+                    selected = st.checkbox(Genres[5])
+                    if selected:
+                        selected_genre.append(Genres[5])     
+
+                    selected = st.checkbox(Genres[6])
+                    if selected:
+                        selected_genre.append(Genres[6])                    
+
+                    selected = st.checkbox(Genres[7])
+                    if selected:
+                        selected_genre.append(Genres[7])
+
+                with col2:
+                    selected = st.checkbox(Genres[8])
+                    if selected:
+                        selected_genre.append(Genres[8])
+
+                    selected = st.checkbox(Genres[9])
+                    if selected:
+                        selected_genre.append(Genres[9])
+
+                    selected = st.checkbox(Genres[10])
+                    if selected:
+                        selected_genre.append(Genres[10])
+
+                    selected = st.checkbox(Genres[11])
+                    if selected:
+                        selected_genre.append(Genres[11])                  
+
+                    selected = st.checkbox(Genres[12])
+                    if selected:
+                        selected_genre.append(Genres[12])                   
+
+                    selected = st.checkbox(Genres[13])
+                    if selected:
+                        selected_genre.append(Genres[13])     
+
+                    selected = st.checkbox(Genres[14])
+                    if selected:
+                        selected_genre.append(Genres[14])                    
+
+                    selected = st.checkbox(Genres[15])
+                    if selected:
+                        selected_genre.append(Genres[15])                   
+
+                if st.button("Valider la saisie"):
+                    writer.writerow([_, _, _, _, _, _, _, selected_genre, _])
+
+            else:
+                st.error("Nom d'utilisateur et/ou mot de passe incorrect(s).")
                 
-        else:
-            st.error("Nom d'utilisateur et/ou mot de passe incorrect(s).")
+if __name__ == '__main__':
+    main()
