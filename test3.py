2e7278136a85afca13d73d82a59fb95eca0c3e8c
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-28 14:39:59 -0400

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,114 +1,81 @@
 import streamlit as st
 import pandas as pd
-import hashlib
+from hashlib import sha256
+import os
 
-def signup():
-    st.subheader("Create New Account")
-    new_user = st.text_input("Enter Username")
-    new_password = st.text_input("Enter Password", type="password")
-    if st.button("Signup"):
-        df = pd.read_csv("users.csv", index_col="Username")
-        if new_user in df.index:
-            st.warning("This username already exists. Please try again with a different username.")
-        else:
-            hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-            df.loc[new_user] = [hashed_password]
-            df.to_csv("users.csv")
-            st.success("You have successfully created a new account. Please proceed to login.")
+USER_CREDS_FILE = "user_creds.csv"
+HASH_ROUNDS = 100000
+
+
+def hash_password(password, salt):
+    return sha256((password + salt).encode()).hexdigest()
+
+
+def create_user(username, password):
+    salt = os.urandom(16).hex()
+    hashed_password = hash_password(password, salt)
+    df = pd.DataFrame({"Username": [username], "Salt": [salt], "HashedPassword": [hashed_password]})
+    df.to_csv(USER_CREDS_FILE, mode="a", header=not os.path.exists(USER_CREDS_FILE), index=False)
+
+
+def check_user_credentials(username, password):
+    df = pd.read_csv(USER_CREDS_FILE)
+    user_row = df.loc[df["Username"] == username]
+    if user_row.empty:
         return False
-    else:
-        return False
+    salt = user_row["Salt"].values[0]
+    hashed_password = user_row["HashedPassword"].values[0]
+    return hashed_password == hash_password(password, salt)
+
 
 def login():
     st.subheader("Login")
-    username = st.text_input("Enter Username")
-    password = st.text_input("Enter Password", type="password")
-    if st.button("Login"):
-        df = pd.read_csv("users.csv", index_col="Username")
-        if username not in df.index:
-            st.warning("This username does not exist. Please try again with a valid username.")
-            return False
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Log In"):
+        if check_user_credentials(username, password):
+            st.success("Logged in!")
+            return True
         else:
-            hashed_password = hashlib.sha256(password.encode()).hexdigest()
-            if hashed_password == df.loc[username][0]:
-                st.success("You have successfully logged in!")
-                return True
-            else:
-                st.warning("Incorrect password. Please try again with a valid password.")
-                return False
-    else:
-        return False
+            st.warning("Incorrect username or password")
+            return False
 
-def main():
-    st.title("My App")
-    menu = ["Home", "Login", "Signup"]
-    choice = st.sidebar.selectbox("Menu", menu)
-    if choice == "Home":
-        st.subheader("Home")
-        st.write("Welcome to my app!")
-    elif choice == "Login":
-        if login():
-            st.subheader("Home")
-            st.write("Welcome to my app!")
-    elif choice == "Signup":
-        signup()
-
-if __name__ == "__main__":
-    main()
-import streamlit as st
-import pandas as pd
-import hashlib
 
 def signup():
-    st.subheader("Create New Account")
-    new_user = st.text_input("Enter Username")
-    new_password = st.text_input("Enter Password", type="password")
-    if st.button("Signup"):
-        df = pd.read_csv("users.csv", index_col="Username")
-        if new_user in df.index:
-            st.warning("This username already exists. Please try again with a different username.")
+    st.subheader("Sign Up")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Sign Up"):
+        df = pd.read_csv(USER_CREDS_FILE)
+        if username in df["Username"].values:
+            st.warning("Username already taken")
         else:
-            hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-            df.loc[new_user] = [hashed_password]
-            df.to_csv("users.csv")
-            st.success("You have successfully created a new account. Please proceed to login.")
-        return False
-    else:
-        return False
+            create_user(username, password)
+            st.success("Account created!")
+            return True
 
-def login():
-    st.subheader("Login")
-    username = st.text_input("Enter Username")
-    password = st.text_input("Enter Password", type="password")
-    if st.button("Login"):
-        df = pd.read_csv("users.csv", index_col="Username")
-        if username not in df.index:
-            st.warning("This username does not exist. Please try again with a valid username.")
-            return False
-        else:
-            hashed_password = hashlib.sha256(password.encode()).hexdigest()
-            if hashed_password == df.loc[username][0]:
-                st.success("You have successfully logged in!")
-                return True
-            else:
-                st.warning("Incorrect password. Please try again with a valid password.")
-                return False
-    else:
-        return False
+
+def home():
+    st.subheader("Home")
+    st.write("Welcome to the Multi-Page App")
+
 
 def main():
-    st.title("My App")
-    menu = ["Home", "Login", "Signup"]
-    choice = st.sidebar.selectbox("Menu", menu)
-    if choice == "Home":
-        st.subheader("Home")
-        st.write("Welcome to my app!")
-    elif choice == "Login":
+    if not os.path.exists(USER_CREDS_FILE):
+        pd.DataFrame({"Username": [], "Salt": [], "HashedPassword": []}).to_csv(USER_CREDS_FILE, index=False)
+
+    st.set_page_config(page_title="Multi-Page App", page_icon=":guardsman:", layout="wide")
+
+    if not st.session_state.get('is_logged_in'):
         if login():
-            st.subheader("Home")
-            st.write("Welcome to my app!")
-    elif choice == "Signup":
-        signup()
+            st.session_state['is_logged_in'] = True
+    else:
+        home()
+
+    if not st.session_state.get('is_logged_in'):
+        if signup():
+            st.session_state['is_logged_in'] = True
+
 
 if __name__ == "__main__":
     main()
