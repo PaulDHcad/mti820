1202dd93164755bdc061ec9e14f223c34ad88d33
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-28 13:27:57 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,56 +1,65 @@
 import streamlit as st
 import pandas as pd
-import os
 import hashlib
+import os
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
 def login_page():
-    st.header("Login")
+    st.title("Login Page")
+    st.write("---")
+    st.write("Please enter your login credentials:")
+    
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
-    credentials = read_credentials()
-    if st.button("Log in"):
-        if username in credentials:
-            if verify_password(password, credentials[username]):
-                st.success("Logged in as {}".format(username))
-                return True
-            else:
-                st.warning("Incorrect password")
+    
+    if st.button("Login"):
+        if not (username and password):
+            st.warning("Please fill in both fields.")
         else:
-            st.warning("Username not found")
-    if st.button("Create account"):
-        create_account_page(credentials)
-    return False
-
-def read_credentials():
-    if 'user_credentials.csv' not in os.listdir():
-        return {}
-    else:
-        df = pd.read_csv('user_credentials.csv', index_col='username')
-        return df['password'].to_dict()
-
-def verify_password(password, hash):
-    return hashlib.sha256(password.encode()).hexdigest() == hash
+            df = pd.read_csv("user_credentials.csv")
+            hashed_password = hash_password(password)
+            if (df['Username']==username).any() and (df['Password']==hashed_password).any():
+                st.success("Login successful!")
+            else:
+                st.warning("Incorrect username or password.")
 
 def create_account_page(credentials):
-    st.header("Create Account")
-    new_username = st.text_input("Username")
-    new_password = st.text_input("Password", type="password")
-    if st.button("Create"):
-        if new_username == "" or new_password == "":
-            st.warning("Username or password is empty")
-        elif new_username in credentials:
-            st.warning("Username already exists")
-        else:
-            credentials[new_username] = hashlib.sha256(new_password.encode()).hexdigest()
-            df = pd.DataFrame({'username': list(credentials.keys()), 'password': list(credentials.values())})
-            df.to_csv('user_credentials.csv', index=False)
-            st.success("Account created successfully!")
-            st.info("Please log in to continue.")
+    st.title("Create Account")
+    st.write("---")
+    st.write("Please enter your desired login credentials:")
+
+    with st.form("create_account_form"):
+        new_username = st.text_input("Username")
+        new_password = st.text_input("Password", type="password")
+        confirm_password = st.text_input("Confirm Password", type="password")
+
+        if st.form_submit_button("Create Account"):
+            if not (new_username and new_password and confirm_password):
+                st.warning("Please fill in all fields.")
+            elif new_username in credentials.keys():
+                st.warning("This username already exists. Please choose a different username.")
+            elif new_password != confirm_password:
+                st.warning("Passwords do not match. Please try again.")
+            else:
+                hashed_password = hash_password(new_password)
+                credentials[new_username] = hashed_password
+                df = pd.DataFrame.from_dict(credentials, orient="index", columns=["Password"])
+                df.index.name = "Username"
+                df.to_csv("user_credentials.csv")
+                st.success("Account created successfully!")
+                st.info("Please log in to your new account.")
 
 def main():
-    st.set_page_config(page_title="Login App")
+    if 'user_credentials.csv' not in os.listdir():
+        df = pd.DataFrame(columns=['Username', 'Password'])
+        df.to_csv('user_credentials.csv', index=False)
+    
+    credentials = pd.read_csv("user_credentials.csv", index_col="Username").to_dict()["Password"]
+    
     if not login_page():
-        st.info("Please log in to continue.")
+        create_account_page(credentials)
 
 if __name__ == '__main__':
     main()
