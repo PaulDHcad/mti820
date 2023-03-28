0a904cf9b64b084cb78426be95ecd4b713250533
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-28 12:49:42 -0400

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,23 +1,43 @@
 import streamlit as st
+from streamlit_tags import st_tags
+from streamlit_ace import st_ace
 
-# Define your Streamlit pages as functions
-def page_home():
-    st.title("Welcome to my app!")
-    # Add any other content you want on this page
+def home():
+    st.title('Home Page')
+    st.write('Welcome to my Streamlit app!')
+    st.write('Use the sidebar to navigate to other pages.')
 
-def page_about():
-    st.title("About me")
-    # Add any other content you want on this page
+def editor():
+    st.title('Editor Page')
+    code = st_ace(value='', language='python', height=300)
+    st.write('You entered the following code:')
+    st.code(code)
 
-# Define a dictionary that maps page names to their corresponding functions
-PAGES = {
-    "Home": page_home,
-    "About": page_about
+def tags():
+    st.title('Tags Page')
+    tags = st_tags(
+        label='Enter some tags:',
+        text='Press enter to add more',
+        value=['python', 'streamlit'],
+        suggestions=['data science', 'machine learning', 'visualization']
+    )
+    st.write('You entered the following tags:')
+    for tag in tags:
+        st.write(tag)
+
+# Define the pages dictionary
+pages = {
+    'Home': home,
+    'Editor': editor,
+    'Tags': tags
 }
 
-# Set up the sidebar navigation
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", list(PAGES.keys()))
+# Set the default page to 'Home'
+default_page = 'Home'
 
-# Call the function corresponding to the selected page name
-PAGES[page]()
+# Create a sidebar with the page options
+st.sidebar.title('Navigation')
+selected_page = st.sidebar.radio('Go to', list(pages.keys()), index=list(pages.keys()).index(default_page))
+
+# Display the selected page
+pages[selected_page]()
