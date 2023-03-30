fd61af41a1254a3e1d1b234bb0af06047011ff27
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-30 19:43:07 -0400

    Update test5.py

--- test5.py
+++ test5.py
@@ -17,41 +17,53 @@ def write_user_data(username, password):
 # Define the Streamlit app
 def app():
     st.title('Sign-up/Login Page')
-    st.write('Enter your credentials below to sign up or log in.')
 
-    # Create input fields for username and password
-    username = st.text_input('Username')
-    password = st.text_input('Password', type='password')
+    # Define the layout for the sign-up and login forms
+    col1, col2 = st.beta_columns(2)
 
-    # Create a button to sign up or log in
-    if st.button('Sign Up'):
-        # Check if username and password fields are not empty
-        if username and password:
-            # Read existing user data from the CSV file
-            user_data = read_user_data()
-            # Check if the username already exists in the CSV file
-            usernames = [user[0] for user in user_data]
-            if username in usernames:
-                st.error('Username already exists. Please choose a different one.')
+    with col1:
+        st.write('Sign Up')
+        # Create input fields for new username and password
+        new_username = st.text_input('New Username')
+        new_password = st.text_input('New Password', type='password')
+
+        # Create a button to sign up
+        if st.button('Sign Up'):
+            # Check if username and password fields are not empty
+            if new_username and new_password:
+                # Read existing user data from the CSV file
+                user_data = read_user_data()
+                # Check if the username already exists in the CSV file
+                usernames = [user[0] for user in user_data]
+                if new_username in usernames:
+                    st.error('Username already exists. Please choose a different one.')
+                else:
+                    # Write the new user credentials to the CSV file
+                    write_user_data(new_username, new_password)
+                    st.success('Successfully signed up. Please log in.')
             else:
-                # Write the new user credentials to the CSV file
-                write_user_data(username, password)
-                st.success('Successfully signed up. Please log in.')
-        else:
-            st.error('Please enter a username and password.')
-    elif st.button('Log In'):
-        # Check if username and password fields are not empty
-        if username and password:
-            # Read existing user data from the CSV file
-            user_data = read_user_data()
-            # Check if the username and password match any of the credentials in the CSV file
-            for user in user_data:
-                if username == user[0] and password == user[1]:
-                    st.success('Successfully logged in.')
-                    return
-            st.error('Invalid username or password.')
-        else:
-            st.error('Please enter a username and password.')
+                st.error('Please enter a username and password.')
+
+    with col2:
+        st.write('Log In')
+        # Create input fields for existing username and password
+        existing_username = st.text_input('Existing Username')
+        existing_password = st.text_input('Existing Password', type='password')
+
+        # Create a button to log in
+        if st.button('Log In'):
+            # Check if username and password fields are not empty
+            if existing_username and existing_password:
+                # Read existing user data from the CSV file
+                user_data = read_user_data()
+                # Check if the username and password match any of the credentials in the CSV file
+                for user in user_data:
+                    if existing_username == user[0] and existing_password == user[1]:
+                        st.success('Successfully logged in.')
+                        return
+                st.error('Invalid username or password.')
+            else:
+                st.error('Please enter a username and password.')
 
 if __name__ == '__main__':
     app()
