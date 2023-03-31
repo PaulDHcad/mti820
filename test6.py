4ddc36ebb4085db5c364d3c596aba6018eb325f4
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-30 22:42:39 -0400

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
-def app():
-    st.set_page_config(page_title="Button Example")
-
-    button_clicked = st.button("Click Me!")
-
-    if button_clicked:
-        st.write("Button clicked! This is the home page.")
+st.write("Hello, world!")
