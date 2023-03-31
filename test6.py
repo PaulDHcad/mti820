9ecdbb2ed167f383a608ba8b2c56e4dc049ea571
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-30 22:43:51 -0400

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.write("Hello, world!")
+def home():
+    st.write("Welcome to the home page!")
+    # You can add your own content here
+
+def app():
+    st.set_page_config(page_title="My Streamlit App")
+
+    # Create a button to show the home page
+    if st.button("Show Home Page"):
+        home()
+
+    # Show the initial button if the home page is not displayed yet
+    if not st.session_state.get("home_displayed", False):
+        st.write("Press the button below to show the home page")
+    else:
+        st.session_state.home_displayed = True
