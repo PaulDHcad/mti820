c23bcd7bef4a767d393893fcf05897738e67dace
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-30 22:03:03 -0400

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,13 +1,9 @@
 import streamlit as st
 
-# Define the Streamlit app
 def app():
     st.set_page_config(page_title="Button Example")
 
-    # Create a button
-    button_clicked = st.button("Click me!")
+    button_clicked = st.button("Click Me!")
 
-    # Check if the button has been clicked
     if button_clicked:
-        # Display a message
-        st.write("Button clicked!")
+        st.write("Button clicked! This is the home page.")
