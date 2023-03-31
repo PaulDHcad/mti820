4c9e58892e910e3f7d61aa93a13f410a2daf3317
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-30 20:07:39 -0400

    Update test5.py

--- test5.py
+++ test5.py
@@ -1,53 +1,48 @@
+import csv
 import streamlit as st
 
-def add_user(username, password):
-    with open("users.csv", "a") as f:
-        f.write(f"{username},{password}\n")
-
-def check_user(username, password):
-    with open("users.csv", "r") as f:
-        for line in f:
-            u, p = line.strip().split(",")
-            if u == username and p == password:
-                return True
-        return False
+def signup_page():
+    st.header("Sign Up")
+    username = st.text_input("Username", key="signup_username")
+    password = st.text_input("Password", type="password", key="signup_password")
+    if st.button("Sign Up"):
+        with open("users.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([username, password])
+        st.success("You have successfully created an account!")
 
 def login_page():
-    logged_in = st.session_state.get('logged_in', False)
-
-    if not logged_in:
-        col1, col2, col3 = st.columns([2, 0.1, 2])
-
-        with col1:
-            st.header("Sign Up")
-            username = st.text_input("Username")
-            password = st.text_input("Password", type="password")
-            if st.button("Sign Up"):
-                if username != "" and password != "":
-                    add_user(username, password)
-                    st.success("You have successfully signed up!")
-                else:
-                    st.warning("Please enter a username and password")
-
-        with col2:
-            st.write("|", width=10, )
-
-        with col3:
-            st.header("Log In")
-            username = st.text_input("Username")
-            password = st.text_input("Password", type="password")
-            if st.button("Log In"):
-                if check_user(username, password):
+    st.header("Log In")
+    username = st.text_input("Username", key="login_username")
+    password = st.text_input("Password", type="password", key="login_password")
+    if st.button("Log In"):
+        with open("users.csv", "r") as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == username and row[1] == password:
                     st.success("You have successfully logged in!")
-                    st.session_state.logged_in = True
-                else:
-                    st.warning("Incorrect username or password")
+                    return True
+        st.warning("Incorrect username or password.")
+    return False
 
-    else:
-        st.title("Welcome to the Home Page")
-        st.write("You are now logged in!")
-        st.write("This is the home page, where you can access all the features of our app.")
+def main():
+    st.set_page_config(page_title="My App", page_icon=":guardsman:", layout="wide")
+    col1, col2, col3 = st.beta_columns([1, 0.1, 1])
+    with col1:
+        if not login_page():
+            signup_page()
+    with col2:
+        st.write("")
+        st.write("---OR---")
+        st.write("")
+    with col3:
+        if not login_page():
+            signup_page()
 
+    if login_page():
+        st.write("")
+        st.write("---Home Page---")
+        # Add your home page content here
 
 if __name__ == "__main__":
-    login_page()
+    main()
