9f3fcb8ce038d8c3f0c14eb4a8edbc28a7cd13db
PaulDHcad <128514731+PaulDHcad@users.noreply.github.com>
2023-03-30 21:50:12 -0400

    Update test6.py

--- test6.py
+++ test6.py
@@ -1,23 +1,14 @@
 import streamlit as st
 
-# Create a boolean variable to control the visibility of the form
-show_form = True
-
-# Define the Streamlit app
 def app():
-    st.set_page_config(page_title="Hiding Form Example", page_icon=":eyes:", layout="wide")
-
-    # Create a form
+    st.set_page_config(page_title="Example App", page_icon=":memo:", layout="wide")
+    
+    show_form = st.button("Show Form")
+    
     if show_form:
-        name = st.text_input("Enter your name")
-        age = st.slider("Enter your age", 0, 100, 25)
-        if st.button("Submit"):
-            # Do something with the form data
-            st.write("Name:", name)
-            st.write("Age:", age)
-            # Hide the form by setting the boolean variable to False
-            show_form = False
-
-    # Show a message after the form is hidden
-    if not show_form:
-        st.write("Thanks for submitting the form!")
+        name = st.text_input("Enter your name:")
+        age = st.slider("Enter your age:", 0, 100)
+        submit = st.button("Submit")
+        
+        if submit:
+            st.write(f"Your name is {name} and you are {age} years old.")
